fccb7e599d5e7524f1d12e3bc096ee8a434a262c
MasiEbadi <ebadi59.m@gmail.com>
2024-09-25 13:57:08 +0330

    first code

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(Math.random()*8);
+const Myfamily=["masi","somayeh","erfan","arshan","manely","yara","hamed","mahan"];
+function lottaryHandler(){
+    document.getElementById("result").style.color="blue";
+    document.getElementById("result").innerText=Myfamily(Math.random()*8);
+}
+console.log("Myfamily"(2));
+console.log(Math.round()(Math.random()*8));
